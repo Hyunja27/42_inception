bf7bc42f495dbcc429067e6b2656a293e7ccad97
Hyunja27 <123gk@naver.com>
2021-10-12 18:53:03 +0900

    fixed_4

--- srcs/requirements/vm_basic_setup.sh
+++ srcs/requirements/vm_basic_setup.sh
@@ -4,5 +4,8 @@ sudo apt-get install docker.io -y
 sudo apt-get install docker-compose -y
 sudo apt-get install make -y
 sudo apt-get install vim -y
+sudo mkdir /home/spark/data
+sudo mkdir /home/spark/data/wp
+sudo mkdir /home/spark/data/db
 sudo chmod 777 /etc/hosts
 sudo echo "127.0.0.1    spark.42.fr" >> /etc/hosts
