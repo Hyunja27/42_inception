ac141342a0266d1f11cc9690e9fd944269d35d91
Hyunja27 <123gk@naver.com>
2021-10-12 18:15:50 +0900

    fixed

--- srcs/requirements/vm_basic_setup.sh
+++ srcs/requirements/vm_basic_setup.sh
@@ -1,8 +1,8 @@
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install docker.io
-sudo apt-get install docker-compose
-sudo apt-get install make
-sudo apt-get install vim
+#sudo apt-get update
+#sudo apt-get upgrade
+sudo apt-get install docker.io -y
+sudo apt-get install docker-compose -y
+sudo apt-get install make -y
+sudo apt-get install vim -y
 sudo chmod 777 /etc/hosts
 sudo echo "127.0.0.1    spark.42.fr" >> /etc/hosts
